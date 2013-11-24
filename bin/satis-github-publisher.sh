ad3856d39655f2778d6d7ed7e03b50e454b5d3ab
Stephan Jorek <stephan.jorek@gmail.com>
2013-11-24 16:50:48 +0100

    updated script

--- bin/satis-github-publisher.sh
+++ bin/satis-github-publisher.sh
@@ -27,9 +27,19 @@ else
 fi
 
 echo "SATIS_NAME    :" ${SATIS_NAME:=${6:-$(php -r 'error_reporting(0); try { echo json_decode(file_get_contents("composer.json"))->name; } catch(Exception $e) {}')}}
-echo "SATIS_PATH    :" ${SATIS_PATH:=${7:-.git/satis}}
-echo "SATIS_FILE    :" ${SATIS_FILE:=${8:-satis.json}}
-echo "SATIS_URL     :" ${SATIS_URL:=${9:-$(php -r 'error_reporting(0); try { echo json_decode(file_get_contents("composer.json"))->homepage; } catch(Exception $e) {}')}}
+if [ -z "${SATIS_NAME}" ] ; then
+	echo "Please add a package name in composer.json." >&2
+	exit 1
+fi
+
+echo "SATIS_URL     :" ${SATIS_URL:=${7:-$(php -r 'error_reporting(0); try { echo json_decode(file_get_contents("composer.json"))->homepage; } catch(Exception $e) {}')}}
+if [ -z "${SATIS_URL}" ] ; then
+	echo "Please add a homepage url in composer.json." >&2
+	exit 1
+fi
+
+echo "SATIS_PATH    :" ${SATIS_PATH:=${8:-.git/satis}}
+echo "SATIS_FILE    :" ${SATIS_FILE:=${9:-satis.json}}
 echo "SATIS_PREFIX  :" ${SATIS_PREFIX:=${10:-/${GITHUB_REPO}}}
 
 # Git spits out status information on $stderr, and we don't want to relay that as an error to the
